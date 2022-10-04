00a2dec891cc8cbfc69fa4fc294cc278e28bad0b
oscarsilvamt <oscar.silva.mt@usa.edu.co>
2022-10-04 18:50:06 -0500

    first commit

---
+++ src/main/java/com/doctors/repository/crudrepository/DoctorCrudRepository.java
@@ -0,0 +1,7 @@
+package com.doctors.repository.crudrepository;
+
+import com.doctors.model.DoctorModel;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DoctorCrudRepository extends CrudRepository<DoctorModel,Integer> {
+}
